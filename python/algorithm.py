d53904c0f65e0a9124ed4aa6db2c11db4f889dbd
Kang Lou <kanglousu@gmail.com>
2018-10-03 16:51:16 -0700

    Leetcode 2: Add Two Numbers

--- python/algorithm.py
+++ python/algorithm.py
@@ -1,22 +1,19 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-'''
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-
-'''
 
 class Solution:
     def __init__(self):
         pass # function bady cannot be empty!
 
+
+    """
+    Using data structure: dictionary, list
+    Using lib func: len(), range()
+    """
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -33,6 +30,33 @@ class Solution:
                 myDict[nums[i]] = i
         return []
 
+    """
+    using class, modular, floor division, conditional assignment
+    """
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(1)
+        cur = dummy
+        carry = 0
+        while (l1 or l2) :
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            sumVal = val1 + val2 + carry
+            mod = sumVal % 10
+            carry = sumVal // 10 # python has divison and floor division
+            # print(sumVal, mod, carry)
+            cur.next = ListNode(mod)
+            cur = cur.next
+            l1 = l1.next if l1 else l1
+            l2 = l2.next if l2 else l2
 
+        if carry : 
+            cur.next = ListNode(1)
+
+        return dummy.next
 
 
--- python/test.py
+++ python/test.py
@@ -1,16 +1,41 @@
 
 from algorithm import Solution
 
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def createLinkList(myArray):
+    dummy = ListNode(1)
+    cur = dummy
+    for n in myArray:
+        cur.next = ListNode(n)
+        cur = cur.next
+    return dummy.next
+
+def printLinkList(l):
+    print("LinkList as below:")
+    while (l != None):
+        print(l.val, end='')
+        print(' ', end='')
+        l = l.next
+
 def main():
     solu = Solution()
 
-    nums = [1,2,3,4,5]
-    target = 7
-    res = solu.twoSum(nums, target)
-    print(res)
-    # print("main!")
+    # # two sum test case
+    # nums = [1,2,3,4,5]
+    # target = 7
+    # res = solu.twoSum(nums, target)
+    # print(res)
 
-
+    # add two number test case
+    l1 = createLinkList([1,2,3])
+    l2 = createLinkList([5,6,7,8])
+    res = solu.addTwoNumbers(l1, l2)
+    printLinkList(res)
 
 main()
 
